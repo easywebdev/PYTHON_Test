073f9f232fec4a85811c43653a3d4ffdd105c268
Fedir <fedir.m.shuryhin@nulp.ua>
2026-02-13 09:19:54 +0200

    Add two functions.

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+def suma(a: float, b: float) -> float:
+    return a + b
+
+def diff(a: float, b: float) -> float:
+    return a - b
+
 def main():
     print("New project")
+
+    a = 4
+    b = 5
+
+    res1 = suma(a, b)
+    res2 = diff(a, b)
+
+    print(res1, res2)
 
 main()
